7618891d6bba4a8e1e2a2c97f7009b911fc2c20a
EraMorgett4 <hsw1568@naver.com>
2024-06-10 09:38:17 +0900

    경로 설정

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # js-homework
 
-### Mission-0의 내용은 main.js 안에 넣어놨습니다.
+### [Mission-0의 내용](https://github.com/EraMorgett4/js-homework/blob/main/mission00/mission00.md)
--- mission00/main.js
+++ mission00/main.js
@@ -10,6 +10,18 @@ function getValueAtObject(obj, key) {
   else return obj[key];
 }
 
+// function getValueAtObject(obj, key) {
+//   if (
+//     typeof key === "string" &&
+//     Object.prototype.toString.call(obj) === "[object Object]"
+//   ) {
+//     for (const value of Object.keys(obj)) {
+//       if (value === key) return obj[value];
+//     }
+//   }
+//   throw new Error("Error !");
+// }
+
 const person = {
   name: "Alice",
   age: 25,
@@ -41,9 +53,60 @@ const getNumberAtArray3 = (arr, index) => {
   else throw new Error("유효하지 않은 인덱스 입니다.");
 };
 
+// function getNumberAtArray(arr, index) {
+//   let num = 0;
+//   if (
+//     Array.isArray(arr) &&
+//     typeof index === "number" &&
+//     arr.length > index &&
+//     index >= 0
+//   ) {
+//     arr.forEach((v, i) => {
+//       if (i === index) num = v;
+//     });
+//   } else {
+//     throw new Error("Error!");
+//   }
+
+//   return num;
+// }
+
 const numbers = [10, 20, 30, 40, 50];
 
 console.log(getNumberAtArray(numbers, 2)); // 30
 console.log(getNumberAtArray(numbers, 4)); // 50
 // console.log(getNumberAtArray(numbers, 5)); // Error!
 // console.log(getNumberAtArray(numbers, -1)); // Error!
+
+//범쌤 코드
+
+function getValueAtObject(obj, key) {
+  if (typeof key !== "string") {
+    throw new TypeError("getValueAtObject 함수의 두 번째 인수는 문자 타입 이어야 합니다.");
+  }
+
+  if (typeof obj === "object") {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      return obj[key];
+    } else {
+      throw new Error(`getValueAtObject 함수의 해당 ${key}가 존재하지 않습니다.`);
+    }
+  } else {
+    throw new TypeError("getValueAtObject 함수의 첫 번째 인수는 객체 타입 이어야 합니다.");
+  }
+}
+
+// 1. arr 변수가 배열인지 확인하기
+// 2. 0보다 크거나 같음 && 배열의 길이보다 작을 때
+
+function getNumberAtArray(arr, index) {
+  if (Array.isArray(arr)) {
+    if (index >= 0 && index < arr.length) {
+      return arr[index];
+    } else {
+      throw new Error("배열에 없는 index입니다.");
+    }
+  } else {
+    throw new TypeError("getNumberAtArray 함수의 첫 번째 인수는 배열 타입 이어야 합니다.");
+  }
+}
